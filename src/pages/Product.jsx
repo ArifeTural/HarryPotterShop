a781e16c0067919d878e37e4aabac02cdbb7c619
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-09-28 00:20:18 +0300

    search bitti

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,20 +9,17 @@ import Form from "react-bootstrap/Form";
 const Product = () => {
   const [search, setSearch] = useState("")
 
-
-  
-
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 
 <Form.Control
         type="search"
         onChange={(e) => setSearch(e.target.value)}
-        placeholder="Oyuncu arama.."
-        className="w-75 mx-auto my-2"
+        placeholder="Ürün arama.."
+        className="w-[75%] h-[50px] font-montserrat  hover:cursor-pointer text-[20px] text-main font-[600] "
       />
 
-      <h2 className="text-2xl font-bold mt-8 tracking-tight text-gray-900">
+      <h2 className="text-2xl font-bold mt-8 tracking-tight text-red-900 text-center text-[35px] mb-4">
         All Products
       </h2>
       <div className='grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8'>
