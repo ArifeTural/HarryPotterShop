627b5eaca5070f41b1e1ccd08dae0cc2907c7065
Q7135 <145381889+Q7135@users.noreply.github.com>
2024-09-27 11:41:27 +0300

    data oluştu

---
+++ src/helper/data.js
@@ -0,0 +1,122 @@
+export const data = [
+    {
+      id: 1,
+      title: "Hogwarts Kar Küresi",
+      category:"accessory",
+      image:"./images/HogwartsKarKuresi699.webp",
+      price:699.99,
+     
+    },
+    {
+      id: 2,
+      title: "Tasarım Kupa",
+      category:"accessory",
+      image:"./images/gozluksimsektasarimliKupa229.webp",
+      price:299.99,
+     
+    },
+    {
+      id: 3,
+      title: "Harry Potter Kitapları Seti",
+      category:"books",
+      image:"./images/kitapSeti1735.webp",
+      price:1734.99,
+     
+    },
+    {
+      id: 4,
+      title: "Seçmen Şapka",
+      category:"accessory",
+      image:"./images/secmensapka499.webp",
+      price:499.99,
+     
+    },
+    {
+      id: 5,
+      title: "Çizgili Defter",
+      category:"books",
+      image:"./images/Deftercizgili149.webp",
+      price:149.99,
+     
+    },
+    {
+      id: 6,
+      title: "Tek Kişilik Nevresim Takımı",
+      category:"accessory",
+      image:"./images/MagicSquadTekNevresim1258.webp",
+      price:1249.99,
+     
+    },
+    {
+      id: 7,
+      title: "Sweatshirt ",
+      category:"clothes",
+      image:"./images/Sweatshirt699.webp",
+      price:699.99,
+     
+    },
+    {
+      id: 8,
+      title: "Fosforlu Kalem",
+      category:"books",
+      image:"./images/fosforlukalem216.webp",
+      price:214.99,
+     
+    },
+    {
+      id: 9,
+      title: "Kapı Önü Paspası",
+      category:"accessory",
+      image:"./images/paspas349.webp",
+      price:349.99,
+     
+    },
+    {
+      id: 10,
+      title: "Baskılı Çorap Seti",
+      category:"clothes",
+      image:"./images/corapSeti269.webp",
+      price:269.99,
+     
+    },
+    {
+      id: 11,
+      title: "Özel Kutulu Metal Asa Seti",
+      category:"accessory",
+      image:"./images/ozelKutuluMetalAsaSeti1500.webp",
+      price:1499.99,
+     
+    },
+    {
+      id: 12,
+      title: "Baskılı Telefon Kılıfı",
+      category:"accessory",
+      image:"./images/telefonkilifi279.webp",
+      price:279.99,
+     
+    },
+    {
+      id: 13,
+      title: "Harry Potter Yemek Kitabı",
+      category:"books",
+      image:"./images/harryPotterYemekKitabi280.webp",
+      price:299.99,
+     
+    },
+    {
+      id: 14,
+      title: "Baskılı Beyaz Tişört",
+      category:"clothes",
+      image:"./images/beyazTisort230.webp",
+      price:229.99,
+     
+    },
+    {
+      id: 15,
+      title: "Hogwarts Kar Küresi",
+      category:"accessory",
+      image:"./images/Kupa330mlLacivert300.webp",
+      price:299.99,
+     
+    },
+]
